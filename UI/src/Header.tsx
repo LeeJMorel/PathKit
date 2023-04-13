2522c69394cb4238054168063e8d1c55b749c12d
Lee Janzen-Morel <leejanzen91@gmail.com>
2023-04-13 13:12:32 -0700

    added the Right Hand modules

--- UI/src/Header.tsx
+++ UI/src/Header.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 
 function Header(props: {
-  customFeatures?: any;
-  column?: any;
-  mode?: any;
+  customFeatures?: string;
+  column?: string;
+  mode?: string;
   searchResults?: any;
 }) {
   // Extract the relevant props
-  const { column, mode, searchResults } = props;
+  const { column, mode, searchResults, customFeatures } = props;
 
   // Conditionally render the header content based on the column
   let headerContent = "";
@@ -17,7 +17,13 @@ function Header(props: {
   } else if (column === "column2") {
     headerContent = "Search";
   } else if (column === "column3") {
-    headerContent = "Custom Features: " + props.customFeatures;
+    headerContent = customFeatures
+      ? customFeatures
+          .split(",")
+          .map((feature) => feature.replace("Module", ""))
+          .filter((feature) => feature !== "Dice")
+          .join(" + ")
+      : "";
   }
 
   return <div className="Header">{headerContent}</div>;
--- UI/src/Right.tsx
+++ UI/src/Right.tsx
@@ -1,7 +1,51 @@
+import React from "react";
 import "./App.css";
 
-function Right() {
-  return <div className="Right"></div>;
+// Import the module components
+import DCModule from "./components/right/DCModule";
+import DiceModule from "./components/right/DiceModule";
+import NotesModule from "./components/right/NotesModule";
+
+interface RightProps {
+  visibleModules: string; // Update the type to accept a string
+}
+
+function Right({ visibleModules }: RightProps) {
+  // Create a mapping of module names to corresponding component references
+  const moduleComponents: { [key: string]: React.FunctionComponent } = {
+    DCModule,
+    DiceModule,
+    NotesModule,
+    // Add more module components and their corresponding names as needed
+  };
+
+  // Render the corresponding module component based on the module names passed down
+  const renderModules = (moduleNames: string) => {
+    // Split the comma-separated string to get an array of module names
+    const modules = moduleNames.split(",");
+
+    // Render each module component in a div container
+    return modules.map((moduleName) => {
+      // Check if the module name is available in the mapping
+      if (moduleComponents.hasOwnProperty(moduleName)) {
+        const ModuleComponent = moduleComponents[moduleName];
+        return (
+          <div key={moduleName} className="RightModule">
+            <ModuleComponent />
+          </div>
+        );
+      } else {
+        // Render a default message or fallback UI if module name is not available
+        return (
+          <div key={moduleName} className="RightModule">
+            No module found for {moduleName}
+          </div>
+        );
+      }
+    });
+  };
+
+  return <div className="Right">{renderModules(visibleModules)}</div>;
 }
 
 export default Right;
