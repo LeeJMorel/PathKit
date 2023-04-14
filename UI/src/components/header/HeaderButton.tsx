ed5ad59fcc18e9724974f6c71f2ac3f246be05fa
Lee Janzen-Morel <leejanzen91@gmail.com>
2023-04-13 17:07:21 -0700

    Delete Old folders

--- UI/src/components/header/HeaderButton.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function HeaderButton() {
-  return <div></div>;
-}
-
-export default HeaderButton;
--- UI/src/components/header/MenuButton.tsx
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function MenuButton() {
-  return <div></div>;
-}
-
-export default MenuButton;
