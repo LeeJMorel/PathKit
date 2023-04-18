c7970fb45366c8d6c92041cd78e779052dfa5797
Lee Janzen-Morel <leejanzen91@gmail.com>
2023-04-18 15:28:29 -0700

    add some header buttons and a menu feature

--- PathKit/src/components/header/HeaderButton.tsx
+++ PathKit/src/components/header/HeaderButton.tsx
@@ -1,7 +1,53 @@
-import "./App.css";
+import React from "react";
 
-function HeaderButton() {
-  return <div></div>;
+interface HeaderButtonProps {
+  type: "close" | "addEntity" | "addPlan";
+  onClick: (isEncounter: boolean) => void;
 }
+
+const HeaderButton: React.FC<HeaderButtonProps> = ({ type, onClick }) => {
+  const handleButtonClick = () => {
+    if (type === "close") {
+      onClick(false);
+    } else if (type === "addEntity") {
+      // Open AddEntityMenu
+      // Implement the logic to show/hide the AddEntityMenu component here
+    } else if (type === "addPlan") {
+      // Open AddPlanMenu
+      // Implement the logic to show/hide the AddPlanMenu component here
+    }
+  };
+
+  let buttonText = "";
+  if (type === "close") {
+    buttonText = "x";
+  } else {
+    buttonText = "+";
+  }
+
+  return (
+    <button
+      onClick={handleButtonClick}
+      style={{
+        borderRadius: "100%",
+        width: "20px",
+        height: "20px",
+        backgroundColor: "gray",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        cursor: "pointer",
+        outline: "none",
+        border: "none",
+        fontSize: "0.8em",
+        fontWeight: "bold",
+        padding: "0",
+      }}
+    >
+      {buttonText}
+    </button>
+  );
+};
 
 export default HeaderButton;
