0248b4c7fb6be6355daedbb9c6c45784d690746d
Lee Janzen-Morel <leejanzen91@gmail.com>
2023-05-10 11:08:58 -0700

    potential dice solution, tauri clean up

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,24 @@
-use crate::db::sqlite::db_init;
-
-pub mod db;
-
 #[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use flate2::read::GzDecoder;
+use std::fs::File;
+use std::io::prelude::*;
+
+#[tauri::command]
+fn load_gzipped_file(file_path: String) -> Vec<u8> {
+    let mut file = File::open(file_path).expect("failed to open file");
+
+    let mut gz = GzDecoder::new(&file);
+    let mut contents = Vec::new();
+    gz.read_to_end(&mut contents).expect("failed to read file");
+
+    contents
+}
+
 #[async_std::main]
 async fn main() {
-    db_init().await;
-
     tauri::Builder::default()
         .plugin(tauri_plugin_sql::Builder::default().build())
         .run(tauri::generate_context!())
